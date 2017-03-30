6ecf9e1e1649622e9dbdb62d9e16888bc8d1102e
lbenedette <blopesleo@outlook.com>
2017-03-29 22:20:20 -0300

    Fix no uso errado de os.environ que deve ser usado como um dicionário [] e não como hash ()

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = os.environ('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SECRET_KEY = 'hellooooooooo'

22201c20bbffa75b5d513f4a968085d83b3e7c5c
Leonardo Lopes <blopesleo@outlook.com>
2017-12-13 23:52:28 -0200

    Remoção de login e usuário da aplicação

--- MPA/app/__init__.py
+++ MPA/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,11 +9,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # database
 db = SQLAlchemy(app)
-# login
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-login_manager.login_message = 'É necessário estar logado para acessar essa página!'
-login_manager.login_message_category = 'warning'
+
 
 from app import views

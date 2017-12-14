3bcbfc47cd4909d7343f2b5b4280387d0c6b8a81
Leonardo Lopes <blopesleo@outlook.com>
2017-12-14 12:07:35 -0200

    Adicionado roteamento utilizando react-route

---
+++ SPA/front/src/Board.js
@@ -0,0 +1,139 @@
+import React, {Component} from 'react';
+import $ from 'jquery';
+
+
+export default class Board extends Component {
+  constructor() {
+    super();
+    this.state = {todo: [], doing: [], done: []};
+  }
+
+  componentDidMount() {
+    $.ajax({
+        url: 'http://127.0.0.1:5000/api/tasks',
+        dataType: 'json',
+        success: function (response) {
+          this.setState({todo: response.todo, doing: response.doing, done: response.done});
+        }.bind(this)
+      }
+    );
+  }
+
+  moveTask(task_id, status, event) {
+    event.preventDefault();
+    $.ajax({
+        url: 'http://127.0.0.1:5000/api/task/update/' + task_id + '/' + status,
+        dataType: 'json',
+        success: function (response) {
+          this.setState({todo: response.todo, doing: response.doing, done: response.done});
+        }.bind(this)
+      }
+    );
+  }
+
+  deleteTask(task_id, status, event) {
+    event.preventDefault();
+    $.ajax({
+        url: 'http://127.0.0.1:5000/api/task/delete/' + task_id,
+        dataType: 'json',
+        success: function (response) {
+          this.setState({[status]: response.tasks});
+        }.bind(this)
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-4">
+            <div className="panel panel-danger">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">
+                  TO DO
+                  <a href="#"><span className="glyphicon glyphicon-plus pull-right" aria-hidden="true"></span></a>
+                </h3>
+              </div>
+              <div className="panel-body">
+                {/*todo itens*/}
+                {
+                  this.state.todo.map(function (task) {
+                    return (
+                      <div className="well well-sm" key={task.id}>
+                        <div className="text-center">
+                          <a onClick={this.deleteTask.bind(this, task.id, 'todo')}><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a>
+                          <a onClick={this.moveTask.bind(this, task.id, 'doing')}><span className="glyphicon glyphicon-circle-arrow-right pull-right" aria-hidden="true"></span></a>
+                        </div>
+                        <div className="text-justify task">
+                          {task.text}
+                        </div>
+                      </div>
+                    )
+                  }.bind(this))
+                }
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel panel-warning">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">
+                  DOING
+                  <a href="#"><span className="glyphicon glyphicon-plus pull-right" aria-hidden="true"></span></a>
+                </h3>
+              </div>
+              <div className="panel-body">
+                {/*doing itens*/}
+                {
+                  this.state.doing.map(function (task) {
+                    return (
+                      <div className="well well-sm" key={task.id}>
+                        <div className="text-center">
+                          <a href="#"><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a>
+                          <a onClick={this.moveTask.bind(this, task.id, 'done')}><span className="glyphicon glyphicon-circle-arrow-right pull-right" aria-hidden="true"></span></a>
+                          <a onClick={this.moveTask.bind(this, task.id, 'todo')}><span className="glyphicon glyphicon-circle-arrow-left pull-left" aria-hidden="true"></span></a>
+                        </div>
+                        <div className="text-justify task">
+                          {task.text}
+                        </div>
+                      </div>
+                    )
+                  }.bind(this))
+                }
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-4">
+            <div className="panel panel-success">
+              <div className="panel-heading">
+                <h3 className="panel-title text-center">
+                  DONE
+                  <a href="#"><span className="glyphicon glyphicon-plus pull-right" aria-hidden="true"></span></a>
+                </h3>
+              </div>
+              <div className="panel-body">
+                {/*done itens*/}
+                {
+                  this.state.done.map(function (task) {
+                    return (
+                      <div className="well well-sm" key={task.id}>
+                        <div className="text-center">
+                          <a href="#"><span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a>
+                          <a onClick={this.moveTask.bind(this, task.id, 'doing')}><span className="glyphicon glyphicon-circle-arrow-left pull-left" aria-hidden="true"></span></a>
+                        </div>
+                        <div className="text-justify task">
+                          {task.text}
+                        </div>
+                      </div>
+                    )
+                  }.bind(this))
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- SPA/front/src/index.js
+++ SPA/front/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import App from './App';
-import './index.css';
+import Board from './Board'
 import './css/bootstrap.min.css'
 import './css/style.css'
 
 ReactDOM.render(
-  <App />,
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Board} />
+      </Route>
+    </Router>
+  ),
   document.getElementById('root')
 );
